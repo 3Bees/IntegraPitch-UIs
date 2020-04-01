85ba7fa8517cc054af078695ce1cc80425c42723
Tasbeel Zahid <47120106+TasbeelZahid@users.noreply.github.com>
2020-04-01 21:40:00 +0500

    update

--- IntegraPitch/App/Navigation/Router.js
+++ IntegraPitch/App/Navigation/Router.js
@@ -30,7 +30,7 @@ const MainStack = createStackNavigator();
 const token = true
 const Main = () => {
     return (
-        <MainStack.Navigator initialRouteName="AuthLoading" screenOptions={{ headerShown: false }} >
+        <MainStack.Navigator initialRouteName="Settings" screenOptions={{ headerShown: false }} >
             <MainStack.Screen name="AuthLoading" component={AuthLoading} />
             <MainStack.Screen name="CreateAccount" component={CreateAccount} />
             <MainStack.Screen name="Settings" component={Settings} />
